10ebdc8ea7245cb61ee6b612bfd551b7b4f446d3
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2022-03-18 14:23:08 +0100

    [LinearGradient]: touch-ups for android

--- src/components/atoms/CollectionRow.js
+++ src/components/atoms/CollectionRow.js
@@ -15,21 +15,22 @@ import { LinearGradient } from 'expo-linear-gradient';
 class CollectionRow extends React.Component {
   render() {
     const {item} = this.props;
+    console.log(item);
     return (
       <View>
        <TouchableOpacity onPress={this.props.onPress} style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
          <Image
-             style={{ flex: 1, height: 250, width: '100%'}}
+             style={styles.collectionImage}
              source={{uri: item.image}}
            />
            <LinearGradient
-            colors={['rgba(0,0,0,0.4)', 'rgba(0, 0, 0, 0.3)', 'transparent']}
+            colors={['rgba(0,0,0,0.6)', 'rgba(0, 0, 0, 0.4)', 'transparent']}
             end={{ x: 1, y: 0 }}
-            locations={[0.0, 0.2, 0.9]}
+            locations={[0.0, 0.2, 1]}
             style={styles.gradient}
             />
          {/* Showcase Title */}
-         <Text>{item.title_es}</Text>
+
        </TouchableOpacity>
       </View>
     );
@@ -37,6 +38,11 @@ class CollectionRow extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  collectionImage: {
+    flex: 1,
+    height: 150,
+    width: '100%'
+  },
   gradient: {
     position: 'absolute',
      left: 0,
